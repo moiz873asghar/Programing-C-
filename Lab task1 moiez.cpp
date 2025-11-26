a050c60c968db58615fce2b9e3d05c7e515c1b5c
moiz873asghar <moiz873asghar@icloud.com>
2025-11-26 08:29:43 +0500

    Add files via upload

---
+++ Lab task1 moiez.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"*****************************************"<<endl;
+	cout<<"*\tName:\t\tXYZ\t\t*"<<endl;
+	cout<<"*\tROLL Number:\t1\t\t*"<<endl;
+	cout<<"*\tDepartment:\tSET\t\t*"<<endl;
+	cout<<"*\tAddress:\tABC\t\t*"<<endl;
+	cout<<"*\tHobby:\t\tcricket\t\t*"<<endl;
+	cout<<"*\tQulifation:\t\t2ndyear\t*"<<endl;
+	cout<<"*\tSSCMarks:\t\t800\t*"<<endl;
+	cout<<"*\tHSSCMark:\t\t720\t*"<<endl;
+	cout<<"*****************************************"<<endl;
+}
